8f14cddd7def7c9a349dc4347422dc963bae9420
David Liu <dliu1236@terpmail.umd.edu>
2025-04-12 16:39:26 -0400

    initial setup

---
+++ main.py
@@ -0,0 +1,28 @@
+from models.church import Model
+
+def main():
+    model = Model()
+    # testSynthetic(model, "./images/church2.jpg")
+    model.setTimestep(175)
+    testRealNoise(model, "./images/night.jpg")
+
+
+def testSynthetic(model, filename):
+    noisy_fname = filename
+    model.loadImageFromFile(noisy_fname)
+    model.normalizeImage()
+    model.testSyntheticNoise(timestep=200)
+    model.displayNoisy()
+    model.diffusion(verbose=True)
+    model.displayDiffused()
+
+def testRealNoise(model, filename):
+    noisy_fname = filename
+    model.loadImageFromFile(noisy_fname)
+    model.normalizeImage()
+    model.displayNoisy()
+    model.diffusion(verbose=True)
+    model.displayDiffused()
+
+if __name__ == "__main__":
+    main()
